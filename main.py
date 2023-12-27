14e7845dcb58f610e06ba3319fc9eba7bc700ffc
axrorovms <msodiq0525@gmail.com>
2023-12-28 00:49:03 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import re
+
+import speech_recognition as sr
+import g4f
+import concurrent.futures
+from gtts import gTTS
+import os
+
+
+def ask_gpt(prompt: str) -> str:
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_4,
+        messages=[{"role": "user", "content": prompt}],
+    )
+    return response
+
+
+
+def process_audio():
+    selected_index = 0
+    selected_mic = sr.Microphone(device_index=selected_index)
+    r = sr.Recognizer()
+
+    with selected_mic as source:
+        print("Ask anything from AI")
+        audio = r.listen(source)
+
+    try:
+        print("Analyzing request")
+        a = r.recognize_google(audio, language='ru-RU')
+        response = ask_gpt(a)
+        print(response)
+        text_to_voice(response)
+    except sr.UnknownValueError:
+        print('Speech could not be understood..\n Please try again(')
+
+    except sr.RequestError as e:
+        print(f'Error occurred: {e}')
+
+
+if __name__ == "__main__":
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(process_audio)
+        future.result()
